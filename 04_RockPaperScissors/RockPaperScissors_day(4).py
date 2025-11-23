500969699a95fa8dd9f8058a856cc1f9f9e0e8ed
Huntermax5 <zaheromar.005@gmail.com>
2025-11-22 23:17:33 -0600

    creating a while loop

--- 04_RockPaperScissors/RockPaperScissors_day(4).py
+++ 04_RockPaperScissors/RockPaperScissors_day(4).py
@@ -11,9 +11,6 @@ print ("Welcome to Rock Paper Scissors")
 
 # =========================================================
 
-# *********** Users input ************:
-
-users_choice = input("1- Rock\n2- Paper\n3- Scissors\nWhat is Your choice: ")
 
 # ****** Visulizing Choices by ASCII art ***********
 
@@ -22,45 +19,58 @@ Paper = logos.Paper
 Scissors = logos.Scissors
 
 # ============= Visualizing Choices ========================
-if users_choice == "1":
-    print(f"Your Choice:\n {Rock}" )
-
-elif users_choice == "2":
-    print(f"Your Choice:\n {Paper}")
+ending = False
+while ending == False:
     
-elif users_choice == "3":
-    print(f"Your Choice:\n {Scissors}")
-# ============= Computer Choice ========================
+# *********** Users input ************:
 
-# ******** Create a list ***********
-
-lis = ["rock","paper","scissors"]
-
-# ******* Randomize list ********
-
-Computer_choice = random.choice(lis)
-
-# ****** Visulizing Computers Choice ***********
-
-if Computer_choice == "rock":
-    print(f"Computer Choice:\n {Rock}" )
-
-elif Computer_choice == "paper":
-    print(f"Computer Choice:\n {Paper}" )
+    users_choice = input("1- Rock\n2- Paper\n3- Scissors\nWhat is Your choice: ")
     
-elif Computer_choice == "scissors":
-    print(f"Computer Choice:\n {Scissors}" )
-# =========================================================    
-    
-# ================ Winning or lossing ============================
+    if users_choice == "1":
+        print(f"Your Choice:\n {Rock}" )
 
-if users_choice == "1" and Computer_choice == "scissors" or users_choice == "2" and Computer_choice == "rock" or users_choice == "3" and Computer_choice == "paper":
-    print (logos.win)
-    
-elif users_choice == "1" and Computer_choice == "paper" or users_choice == "2" and Computer_choice == "scissors" or users_choice == "3" and Computer_choice == "rock":
-    print (logos.lose)
+    elif users_choice == "2":
+        print(f"Your Choice:\n {Paper}")
+        
+    elif users_choice == "3":
+        print(f"Your Choice:\n {Scissors}")
+    # ============= Computer Choice ========================
 
-else:
-    print (logos.draw)
+    # ******** Create a list ***********
+
+    lis = ["rock","paper","scissors"]
+
+    # ******* Randomize list ********
+
+    Computer_choice = random.choice(lis)
+
+    # ****** Visulizing Computers Choice ***********
+
+    if Computer_choice == "rock":
+        print(f"Computer Choice:\n {Rock}" )
+
+    elif Computer_choice == "paper":
+        print(f"Computer Choice:\n {Paper}" )
+        
+    elif Computer_choice == "scissors":
+        print(f"Computer Choice:\n {Scissors}" )
+    # =========================================================    
+        
+    # ================ Winning or lossing ============================
+
+    if users_choice == "1" and Computer_choice == "scissors" or users_choice == "2" and Computer_choice == "rock" or users_choice == "3" and Computer_choice == "paper":
+        print (logos.win)
+        
+    elif users_choice == "1" and Computer_choice == "paper" or users_choice == "2" and Computer_choice == "scissors" or users_choice == "3" and Computer_choice == "rock":
+        print (logos.lose)
+
+    else:
+        print (logos.draw)
     
+    user_input = input("Do you want to play again? (yes/no): ").lower()
+    if user_input == 'yes':
+        continue
+    else:
+        ending = True
+        print("Goodbye!")
 # =========================================================

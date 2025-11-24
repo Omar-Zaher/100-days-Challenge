e435d4db36e5d620321dd950d9ec6988ba5770f9
Huntermax5 <zaheromar.005@gmail.com>
2025-11-24 16:34:50 -0600

    Day 16

---
+++ 16_improved_CoffeeMachine/main.py
@@ -0,0 +1,61 @@
+# ===================================================== Improved Coffee Machine =====================================================
+
+# ------------ Imports ------------
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+from prettytable import PrettyTable
+
+# ------------ Variables ------------
+money = MoneyMachine()
+table = PrettyTable()
+menu = Menu()
+choices = [1,2,3]
+items = menu.get_items()
+maker = CoffeeMaker()
+table.add_column("Numbers",choices)
+table.add_column("Coffee",items)
+
+# =================================================== Main Program ===================================================
+while True:
+    print(table)
+    # ------------ User Choice ------------
+    while True: 
+            try:
+                user_choice = int(input("Select a drink number: "))
+                if user_choice not in choices:
+                    print("Please select a valid drink number from the menu.")
+                    continue
+                break
+            except ValueError:
+                print("Invalid input. Please enter a number corresponding to your drink choice.") 
+
+    # ------------ Process Order ------------
+    if user_choice == 1:
+        drink = MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5)    
+    elif user_choice == 2:
+        drink = MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5) 
+    else:
+        drink = MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3)    
+              
+    # ------------ Check Resources & Make Coffee ------------
+    if maker.is_resource_sufficient(drink):
+        maker.make_coffee(drink)
+        print(f"That will be ${drink.cost}")
+        money.make_payment(drink.cost)
+        while True: 
+            more = input("Would you like to order something else: ").lower()
+            if more == "yes":
+                break
+            elif more == "no":
+                money.report()
+                print("I hope you have a good day then!")
+                break
+            else:
+                print("Please enter yes or no!\n")
+        
+        if more == "no":
+            break
+    else:
+        break
+        
---
+++ 16_improved_CoffeeMachine/practice.py
@@ -0,0 +1,19 @@
+# import turtle
+
+# t1 = turtle.Turtle()
+# print(t1)
+# t1.shape("turtle")
+# t1.color("Green")
+# my_screen = turtle.Screen()
+# #print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+
+table.add_column("Numbers",[1,2,3,4,5])
+table.add_column("Coffee", ["Espresso","Americano","Latte","Cappuccino","Mocha"])
+
+print(table)

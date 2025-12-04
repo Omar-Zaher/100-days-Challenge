75f95e72244838044c4160dd8bb89044ec57e355
Huntermax5 <zaheromar.005@gmail.com>
2025-12-04 14:03:51 -0500

    Screen Setup

--- 18_HirstPainting/main.py
+++ 18_HirstPainting/main.py
@@ -6,6 +6,7 @@ import turtle as t
 t1 = t.Turtle()
 screen = t.Screen()
 screen.colormode(255)
+screen.setup(width= 580, height=580)
 t1.speed("fastest")
 
 color_lis = [(133, 164, 202), (225, 150, 101), (30, 43, 64), (201, 136, 148), (163, 59, 49), (236, 212, 88), (44, 101, 147), (136, 181, 161), (148, 64, 72), (51, 41, 45), (161, 32, 29), (60, 115, 99), (59, 48, 45), (170, 29, 32), (215, 83, 73), (236, 167, 157), (230, 163, 168), (36, 61, 55), (15, 96, 71), (33, 60, 106), (172, 188, 219), (194, 99, 108), (106, 126, 158), (18, 83, 105), (175, 200, 188), (35, 150, 209)]
@@ -17,7 +18,7 @@ def draw_dots():
     t1.setheading(270) # Pointing downwards
     t1.forward(250)
     t1.setheading(180)
-    t1.forward(200)
+    t1.forward(232.5)
     t1.setheading(0)
     
     # Drawing 10x10 grid of dots

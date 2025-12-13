ddf660b873fc14917d9dad4871baddc4014f607e
Huntermax5 <zaheromar.005@gmail.com>
2025-12-13 17:45:49 +0400

    High Score

--- 20/21_SnakeGame/scoreboard.py
+++ 20/21_SnakeGame/scoreboard.py
@@ -4,6 +4,7 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.update_score()
     
     def update_score(self):
@@ -21,11 +22,26 @@ class ScoreBoard(Turtle):
         self.update_score()
         
     def end(self):
+        try:
+            with open("data.txt", mode="r") as data:
+                content = data.read().strip()
+                old_high_score = int(content) if content else 0
+
+        except FileNotFoundError:
+            old_high_score = 0
+        if self.score > self.high_score and self.score > old_high_score:
+            self.high_score = self.score
+        else:
+            self.high_score = old_high_score   
+             
+        with open("data.txt", mode="w") as data:
+            data.write(f"{self.high_score}")
+        
         self.clear()
         self.home()
         self.write("GAME OVER!", align= "center", font= ("Arial", 24, "normal"))
         self.goto(0,-20)
-        self.write(f"Your Score: {self.score}", align= "center", font= ("Arial", 13, "normal"))
+        self.write(f"Your High Score: {self.high_score}", align= "center", font= ("Arial", 13, "normal"))
         
     def reset(self):
         self.clear()

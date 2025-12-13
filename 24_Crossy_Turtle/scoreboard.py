a0ba7cc2740ee503ce9570e1b589c4a9d3d31b0b
Huntermax5 <zaheromar.005@gmail.com>
2025-12-13 18:55:58 +0400

    High Score

--- 24_Crossy_Turtle/scoreboard.py
+++ 24_Crossy_Turtle/scoreboard.py
@@ -3,14 +3,15 @@ from turtle import Turtle
 class ScoreBoard(Turtle):
     def __init__(self,x,y):
         super().__init__()
+        self.penup()
         self.score = 0
+        self.high_score = 0
         self.goto(x, y)
         self.update_score()
     
     def update_score(self):
         self.hideturtle()
         self.color("white")
-        self.penup()
         self.speed("fastest")
         self.write(f"{self.score}", align= "center", font= ("Arial", 20, "normal"))
     
@@ -21,11 +22,26 @@ class ScoreBoard(Turtle):
         self.update_score()
         
     def end(self):
+        try:
+            with open("data.txt", mode="r") as data:
+                content = data.read().strip()
+                old_high_score = int(content) if content else 0
+
+
+        except FileNotFoundError:
+            old_high_score = 0
+        if self.score > self.high_score and self.score > old_high_score:
+            self.high_score = self.score
+        else:
+            self.high_score = old_high_score   
+             
+        with open("data.txt", mode="w") as data:
+            data.write(f"{self.high_score}")
         self.clear()
         self.goto(0,260)
         self.write("GAME OVER!", align= "center", font= ("Arial", 24, "normal"))
         self.goto(0,240)
-        self.write(f"Your Score: {self.score}", align= "center", font= ("Arial", 13, "normal"))
+        self.write(f"Your High Score: {self.high_score}", align= "center", font= ("Arial", 13, "normal"))
         
     def reset(self):
         self.clear()

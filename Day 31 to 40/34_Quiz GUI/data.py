64a072d836a973858afa98ac7d920632a96eccc5
Omar Zaher <zaheromar.005@gmail.com>
2026-01-31 21:50:19 -0500

    Delete Day 31 to 40 directory

--- Day 31 to 40/34_Quiz GUI/data.py
+++
@@ -1,8 +0,0 @@
-import requests as rq
-
-
-response = rq.get("https://opentdb.com/api.php", params={"amount": 11,"type": "boolean"})
-response.raise_for_status()
-data = response.json()
-
-question_data = data["results"]
